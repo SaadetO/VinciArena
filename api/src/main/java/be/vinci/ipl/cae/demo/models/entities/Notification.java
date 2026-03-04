e96f5d875e9e9e40dba48b689087a4bedda1642b
ozan <saadet.ozan@student.vinci.be>
2026-03-04 15:31:19 +0100

    feat(notification): add Notification model

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/entities/Notification.java
@@ -0,0 +1,42 @@
+package be.vinci.ipl.cae.demo.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@NoArgsConstructor
+public class Notification {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long idNotification;
+
+  @ManyToOne(fetch= FetchType.LAZY)
+  @JoinColumn(name= "member_id")
+  @JsonBackReference
+  private Member member;
+
+  @Column(nullable = false)
+  private String content;
+
+  @Column(nullable = false)
+  private boolean isRead;
+
+  @Column(nullable = false)
+  private LocalDateTime dateTime;
+
+
+}
