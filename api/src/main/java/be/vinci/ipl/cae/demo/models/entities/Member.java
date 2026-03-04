e30f219bb84daeb4b8059036f92f507a8a0a9e69
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-03-04 16:57:50 +0100

    feat: add entities member and speciality

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/entities/Member.java
@@ -0,0 +1,59 @@
+package be.vinci.ipl.cae.demo.models.entities;
+
+import be.vinci.ipl.cae.demo.models.entities.Speciality;
+import be.vinci.ipl.cae.demo.models.entities.Team;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.awt.Image;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+    name = "members",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"tag", "id_team"})
+    }
+)
+@Data
+@NoArgsConstructor
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long idMember;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  private String tag;
+
+  @ManyToOne
+  @JoinColumn(name = "id_speciality")
+  private Speciality speciality;
+
+  private boolean admin;
+
+  @ManyToOne
+  @JoinColumn(name = "id_team", nullable = true)
+  private Team team;
+
+  private LocalDateTime creationDate;
+
+  @ManyToOne
+  @JoinColumn(name = "id_image")
+  private Image imageProfile;
+
+  private boolean deleted;
+
+}
---
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/entities/Team.java
@@ -0,0 +1,14 @@
+package be.vinci.ipl.cae.demo.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "teams")
+@Data
+@NoArgsConstructor
+public class Team {
+
+}
