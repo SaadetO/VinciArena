44f4f70c8d63b8f7335d8fe953f76dc207b8793b
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-05 23:33:23 +0100

    feat: add MantToMany relation to MatchLineup. MatchLineup now has members attribute

--- api/src/main/java/be/vinci/ipl/cae/demo/models/entities/MatchLineup.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/entities/MatchLineup.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -37,4 +41,19 @@ public class MatchLineup {
   @Column(nullable = false)
   private boolean isRetreated = false;
 
+  @ManyToMany
+  @JoinTable(name = "match_members", joinColumns = @JoinColumn(name = "match_lineup_id"), inverseJoinColumns = @JoinColumn(name = "Member_id"))
+  private Set<Member> members = new HashSet<>();
+
+  /*
+  Adds member to the lineup only if there are less than 4 members in it.
+  Returns false if member wasn't added
+   */
+  boolean addMember(Member member) {
+    if (this.members.size() < 4) {
+      members.add(member);
+      return true;
+    }
+    return false;
+  }
 }
