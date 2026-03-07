301610a25fcb916b12d18d0b8ef027c1af35df05
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-07 16:01:52 +0100

    fix: fix incorrect Mapping Error in ResultMatch and fix incorrect method name in NotificationsRepo

--- api/src/main/java/be/vinci/ipl/cae/demo/DemoApplication.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package be.vinci.ipl.cae.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 /**
  * Main class of the application.
@@ -16,6 +18,10 @@ public class DemoApplication {
    * @param args the arguments
    */
   public static void main(String[] args) {
+
     SpringApplication.run(DemoApplication.class, args);
   }
+
+  //default values for test
+
 }
