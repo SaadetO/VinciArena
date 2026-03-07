03ff27dd20500582b562d6c48b50e5f75da2e1b0
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-03-07 19:44:51 +0100

    feat(team): add newTeam DTO

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/dtos/NewTeam.java
@@ -0,0 +1,16 @@
+package be.vinci.ipl.cae.demo.models.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * NewTeam DTO.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class NewTeam {
+
+  private String name;
+}
