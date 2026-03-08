bac0a72e0dfe36aa6d5090ef0e8de55b77b5b97e
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-08 16:11:05 +0100

    feat/docs/refactor: new methods in NotificationRepository, completed documentation, added orderBy to an existing method

--- api/src/main/java/be/vinci/ipl/cae/demo/repositories/NotificationRepository.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/repositories/NotificationRepository.java
@@ -1,10 +1,38 @@
 package be.vinci.ipl.cae.demo.repositories;
 
-
 import be.vinci.ipl.cae.demo.models.entities.Notification;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository interface for Notification entities.
+ * Provides specialized query methods for member-specific notification management.
+ */
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
 
-  Iterable<Notification> findByMemberIdMember(Long idMember);
+  /**
+   * Retrieves all notifications for a specific member, prioritized by read status.
+   * Unread notifications (isRead=false) appear first, followed by read notifications.
+   * Results are further sorted by the creation date in descending order.
+   *
+   * @param idMember the unique identifier of the member.
+   * @return an iterable of notifications sorted by read status and date.
+   */
+  Iterable<Notification> findByMemberIdMemberOrderByIsReadAscDateTimeDesc(Long idMember);
+
+  /**
+   * Retrieves only the notifications that have not been read yet for a specific member.
+   *
+   * @param idMember the unique identifier of the member.
+   * @return an iterable containing only unread notifications.
+   */
+  Iterable<Notification> findByMemberIdMemberAndIsReadFalse(Long idMember);
+
+  /**
+   * Calculates the total count of unread notifications for a specific member.
+   * This is used to display the numerical badge on the notification bell.
+   *
+   * @param idMember the unique identifier of the member.
+   * @return the total number of unread notifications.
+   */
+  long countMemberIdMemberAndIsReadFalse(Long idMember);
 }
