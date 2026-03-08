ccfa1fd2e389b1d8fe77f94e10d899ee493a9593
NiteChill <puissantachille@gmail.com>
2026-03-08 23:05:30 +0100

    style(api): fix checkstyle warning

--- api/src/main/java/be/vinci/ipl/cae/demo/DemoApplication.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/DemoApplication.java
@@ -29,45 +29,78 @@ public class DemoApplication {
 
   // default values for test
   @Bean
-  CommandLineRunner seed(MemberRepository memberRepo, NotificationRepository notifsRepo) {
+  CommandLineRunner seed(
+      MemberRepository memberRepo, 
+      NotificationRepository notifsRepo,
+      be.vinci.ipl.cae.demo.repositories.TeamRepository teamRepo,
+      be.vinci.ipl.cae.demo.repositories.SpecialityRepository specRepo) {
     return args -> {
+      // Create Specialities
+      String[] specialities = {
+          "architecte",
+          "exécuteur",
+          "tacticien",
+          "gardien",
+          "catalyseur",
+          "perturbateur",
+          "guérisseur"
+      };
+      java.util.Map<String, be.vinci.ipl.cae.demo.models.entities.Speciality> specMap =
+          new java.util.HashMap<>();
+      for (String specialty : specialities) {
+        be.vinci.ipl.cae.demo.models.entities.Speciality spec =
+            new be.vinci.ipl.cae.demo.models.entities.Speciality();
+        spec.setName(specialty);
+        specMap.put(specialty, specRepo.save(spec));
+      }
+
+      // Create Member 1: Manager
       Member member1 = new Member();
-      member1.setEmail("alice@vinci.be");
-      member1.setPassword("hashed_password_123");
-      member1.setTag("AliceV");
+      member1.setEmail("larry@cae.com");
+      member1.setPassword("$2y$10$EW75BdcbCfKEA3NbE7in4OiYYacfP3OY4Q2JsVzjxd3jNxxgAEZNu");
+      member1.setTag("Larry");
       member1.setAdmin(true);
       member1.setDeleted(false);
-      memberRepo.save(member1);
-      // (Assumes you have saved team/speciality/profileImage or they are null)
+      member1.setSpeciality(specMap.get("architecte"));
+      member1 = memberRepo.save(member1);
 
-      // --- Create Member 2: The Player ---
+      // Create Member 2: Player
       Member member2 = new Member();
-      member2.setEmail("bob@vinci.be");
-      member2.setPassword("hashed_password_456");
-      member2.setTag("BobTheBuilder");
+      member2.setEmail("barry@cae.com");
+      member2.setPassword("$2y$10$EW75BdcbCfKEA3NbE7in4OiYYacfP3OY4Q2JsVzjxd3jNxxgAEZNu");
+      member2.setTag("Barry");
       member2.setAdmin(false);
       member2.setDeleted(false);
+      member2.setSpeciality(specMap.get("exécuteur"));
+      member2 = memberRepo.save(member2);
+
+      // Create Team
+      be.vinci.ipl.cae.demo.models.entities.Team team1 =
+          new be.vinci.ipl.cae.demo.models.entities.Team();
+      team1.setName("M8");
+      team1.setIsActive(true);
+      team1.setManager1(member1);
+      team1 = teamRepo.save(team1);
+
+      // Link members to team
+      member1.setTeam(team1);
+      member2.setTeam(team1);
+      memberRepo.save(member1);
       memberRepo.save(member2);
 
-      // Create Notifications for Alice ---
+      // Create Notifications for Member 1
       Notification notif1 = new Notification();
       notif1.setMember(member1);
-      notif1.setContent("Welcome to the team, Alice!");
+      notif1.setContent("Welcome to the team, Larry!");
       notif1.setRead(false);
       notifsRepo.save(notif1);
 
+      // Create Notification for Member 2
       Notification notif2 = new Notification();
       notif2.setMember(member2);
-      notif2.setContent("Your match result has been confirmed.");
+      notif2.setContent("Your match result has been confirmed, Barry.");
       notif2.setRead(false);
       notifsRepo.save(notif2);
-
-      // Create Notification for Bob ---
-      Notification notif3 = new Notification();
-      notif3.setMember(member2);
-      notif3.setContent("New match challenge received!");
-      notif3.setRead(false);
-      notifsRepo.save(notif3);
     };
   }
 
