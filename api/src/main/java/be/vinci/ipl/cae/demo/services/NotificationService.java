e18be5ed4a179cdb1e72a7de03ffdee70074cbb7
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-10 10:43:59 +0100

    feat:add authorize constraint for Notifications. A member can only access their own notifications

--- api/src/main/java/be/vinci/ipl/cae/demo/services/NotificationService.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/services/NotificationService.java
@@ -5,6 +5,7 @@ import be.vinci.ipl.cae.demo.models.entities.Notification;
 import be.vinci.ipl.cae.demo.repositories.MemberRepository;
 import be.vinci.ipl.cae.demo.repositories.NotificationRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,6 +28,7 @@ public class NotificationService {
     this.memberRepository = memberRepository;
     this.notificationRepository = notificationRepository;
   }
+
 
   /**
    * Inserts new notification in to the db.
@@ -81,6 +83,10 @@ public class NotificationService {
    */
   public long countUnreadNotifications(long idMember) {
     return notificationRepository.countByMemberIdMemberAndIsReadFalse(idMember);
+  }
+
+  public Optional<Notification> getById(Long idNotification) {
+    return notificationRepository.getNotificationByIdNotification(idNotification);
   }
 }
 
