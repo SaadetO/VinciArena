a28df4250c30d59897a1c055f2782e7dec2a6143
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-10 12:20:12 +0100

    create service methods to be used in case of notification

--- api/src/main/java/be/vinci/ipl/cae/demo/services/NotificationService.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/services/NotificationService.java
@@ -2,9 +2,13 @@ package be.vinci.ipl.cae.demo.services;
 
 import be.vinci.ipl.cae.demo.models.entities.Member;
 import be.vinci.ipl.cae.demo.models.entities.Notification;
+import be.vinci.ipl.cae.demo.models.entities.Team;
 import be.vinci.ipl.cae.demo.repositories.MemberRepository;
 import be.vinci.ipl.cae.demo.repositories.NotificationRepository;
+import be.vinci.ipl.cae.demo.repositories.TeamRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +19,7 @@ public class NotificationService {
 
   private final MemberRepository memberRepository;
   private final NotificationRepository notificationRepository;
+  private final TeamRepository teamRepository;
 
   /**
    * Creates the NotificationService.
@@ -23,19 +28,21 @@ public class NotificationService {
    * @param notificationRepository notification repository
    */
   public NotificationService(MemberRepository memberRepository,
-      NotificationRepository notificationRepository) {
+      NotificationRepository notificationRepository,
+      TeamRepository teamRepository) {
     this.memberRepository = memberRepository;
     this.notificationRepository = notificationRepository;
+    this.teamRepository = teamRepository;
   }
 
   /**
-   * Inserts new notification in to the db.
+   * Inserts new notification in to the db for one member.
    *
    * @param idMember = id of member who owns the notification
    * @param content  = text message that the notification contains
    * @return new Notification
    */
-  public void createNotificationForMember(Long idMember, String content) {
+  public void notifyMember(Long idMember, String content) {
     if (content.isBlank()) {
       throw new IllegalArgumentException("content must contain text");
     }
@@ -48,7 +55,35 @@ public class NotificationService {
     notificationRepository.save(newNotification);
   }
 
+  /**
+   * Inserts new notification in to the db for all the members isDeleted = false.
+   *
+   * @param content message of the notification
+   */
+  public void notifyAllMembers(String content) {
+    Member[] activeMembers = memberRepository.getAllByIsDeleted(false);
+    for (Member activeMember : activeMembers) {
+      notifyMember(activeMember.getIdMember(), content);
+    }
+  }
 
+  public void notifyTeam(Team team, String content) {
+    List<Member> teamMembers = team.getMembers();
+    for (Member teamMember : teamMembers) {
+      notifyMember(teamMember.getIdMember(), content);
+    }
+  }
+
+  public void notifyTeamManagers(Team team, String content) {
+    Member manager1 = team.getManager1();
+    Member manager2 = team.getManager2();
+    if (manager1 != null) {
+      notifyMember(manager1.getIdMember(), content);
+    }
+    if (manager2 != null) {
+      notifyMember(manager2.getIdMember(), content);
+    }
+  }
 
   /**
    * Get a notification by its id.
@@ -84,6 +119,11 @@ public class NotificationService {
   public long countUnreadNotifications(long idMember) {
     return notificationRepository.countByMemberIdMemberAndIsReadFalse(idMember);
   }
+
+  public Optional<Notification> getById(Long idNotification) {
+    return notificationRepository.getNotificationByIdNotification(idNotification);
+  }
+
 }
 
 
