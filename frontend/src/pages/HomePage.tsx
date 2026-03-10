3d2ca6f88a76be3f0c8ce8ff945c80ed9855054d
NiteChill <puissantachille@gmail.com>
2026-03-10 12:31:46 +0100

    style(frontend): apply prettier format

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
-import { UserContextType } from "../types";
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import { UserContextType } from '../types';
 
 export const HomePage = () => {
   const { authenticatedUser } = useContext<UserContextType>(UserContext);
-  return <>Home
-    {
-      authenticatedUser && <Link to={`/users/${authenticatedUser.id}`}>Profile</Link>
-    }
-  </>;
+  return (
+    <>
+      Home
+      {authenticatedUser && (
+        <Link to={`/users/${authenticatedUser.id}`}>Profile</Link>
+      )}
+    </>
+  );
 };
