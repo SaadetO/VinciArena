dd696b6762b28c7cc6444c2372af5d796b64d513
NiteChill <puissantachille@gmail.com>
2026-03-10 19:46:28 +0100

    refactor(ZoomTransition): externalize component ZoomTransition

--- frontend/src/themes.tsx
+++ frontend/src/themes.tsx
@@ -1,26 +1,6 @@
 import { CloseRounded } from '@mui/icons-material';
-import { Zoom } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import { TransitionProps } from '@mui/material/transitions';
-import { forwardRef, ReactElement, Ref } from 'react';
-
-const ZoomTransition = forwardRef(function Transition(
-  props: TransitionProps & {
-    children: ReactElement;
-  },
-  ref: Ref<unknown>,
-) {
-  return (
-    <Zoom
-      easing={{
-        enter: 'cubic-bezier(0.16, 1.0, 0.3, 1.0)',
-        exit: 'cubic-bezier(0.7, 0, 0.84, 0)',
-      }}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+import { ZoomTransition } from './components/ZoomTransition';
 
 declare module '@mui/material/styles' {
   interface TypeBackground {
