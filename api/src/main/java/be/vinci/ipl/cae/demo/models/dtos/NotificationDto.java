68d6d7c95cc5de78fa4b6fdc5d55ba287d58b341
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-11 14:53:22 +0100

    fix: unused import

--- api/src/main/java/be/vinci/ipl/cae/demo/models/dtos/NotificationDto.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/models/dtos/NotificationDto.java
@@ -1,6 +1,5 @@
 package be.vinci.ipl.cae.demo.models.dtos;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record NotificationDto(long idNotification, String content, boolean isRead,
