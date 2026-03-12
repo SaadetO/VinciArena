061186bd955a24b318cdff0d01d8db089425dd04
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-12 22:02:28 +0100

    feat: inserted profileImages into the database

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/repositories/ProfileImageRepository.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.repositories;
+
+import be.vinci.ipl.cae.demo.models.entities.ProfileImage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfileImageRepository extends CrudRepository<ProfileImage, Long> {
+
+}
