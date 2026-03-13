aaf64ae7f3410651728ee870c3be3fe1b58ed1c5
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-03-13 16:23:06 +0100

    feat(TeamPage): implement team fetching logic and link team page components

--- frontend/src/pages/TeamPage/index.tsx
+++ frontend/src/pages/TeamPage/index.tsx
@@ -1,13 +1,18 @@
-import { Container, Grid2, Stack, Typography } from '@mui/material';
-import { useContext, useEffect, useState } from 'react';
+import { Container, Grid2, Snackbar, Stack, Typography } from '@mui/material';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import { TeamDetailsInfoDto } from '../../types';
 import { NotFoundPage } from '../NotFoundPage';
 import { TeamBanner } from './components/TeamBanner';
+import { ManagerCard } from './components/ManagerCard';
+import { MembersCard } from './components/MembersCard';
+import { JoinRequestsCard } from './components/JoinRequestsCard';
 
 export const TeamPage = () => {
+  const [snackBarText, setSnackBarText] = useState<string | null>(null);
   const { id } = useParams();
+  const [isLoading, setIsloading] = useState(false);
   const idNbr = Number(id);
   const { authenticatedUser } = useContext(UserContext);
   const [team, setTeam] = useState<TeamDetailsInfoDto | undefined>(undefined);
@@ -15,40 +20,45 @@ export const TeamPage = () => {
     { code: number; message: string; subtitle?: string } | undefined
   >(undefined);
 
+  const fetchTeam = useCallback(async () => {
+    if (isNaN(idNbr) || idNbr <= 0) return;
+    setIsloading(true);
+    let response: Response | undefined = undefined;
+    try {
+      response = await fetch(`/api/teams/${idNbr}/details`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: authenticatedUser?.token ?? '',
+        },
+      });
+      if (response.status === 404) {
+        return setError({
+          code: 404,
+          message: 'Équipe introuvable',
+          subtitle:
+            "La team que vous cherchez n'existe pas ou a été désactivée.",
+        });
+      }
+      if (!response.ok) throw new Error('Failed to fetch team details');
+
+      setTeam(await response.json());
+    } catch (err) {
+      setError({
+        code: response?.status ?? 500,
+        message: 'Une erreur est survenue',
+        subtitle:
+          'Une erreur est survenue lors de la récupération des détails de la team.',
+      });
+    } finally {
+      setIsloading(false);
+    }
+  }, [idNbr, authenticatedUser]);
+
   useEffect(() => {
     setTeam(undefined);
     setError(undefined);
-    if (isNaN(idNbr) || idNbr <= 0) return;
-    (async () => {
-      let response: Response | undefined = undefined;
-      try {
-        response = await fetch(`/api/teams/${idNbr}/details`, {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: authenticatedUser?.token ?? '',
-          },
-        });
-        if (response.status === 404) {
-          return setError({
-            code: 404,
-            message: 'Équipe introuvable',
-            subtitle:
-              "La team que vous cherchez n'existe pas ou a été désactivée.",
-          });
-        }
-        if (!response.ok) throw new Error('Failed to fetch team details');
-
-        setTeam(await response.json());
-      } catch (err) {
-        setError({
-          code: response?.status ?? 500,
-          message: 'Une erreur est survenue',
-          subtitle:
-            'Une erreur est survenue lors de la récupération des détails de la team.',
-        });
-      }
-    })();
-  }, [idNbr, authenticatedUser]);
+    fetchTeam();
+  }, [idNbr, authenticatedUser, fetchTeam]);
 
   if (error) return <NotFoundPage error={error} />;
 
@@ -78,13 +88,26 @@ export const TeamPage = () => {
           </Grid2>
           <Grid2 size={{ xs: 12, lg: 5 }}>
             <Stack spacing="1.5rem">
-              {/* <ManagerCard team={team} /> */}
-              {/* <MembersCard*/}
-              {/* <JoinRequestCardCard*/}
+              <ManagerCard team={team} />
+              <MembersCard isLoading={isLoading} team={team} />
+              {team?.managers.find((e) => e.id === authenticatedUser?.id) && (
+                <JoinRequestsCard
+                  isLoading={isLoading}
+                  team={team}
+                  showNotification={(msg) => setSnackBarText(msg)}
+                  onActionSuccess={fetchTeam}
+                />
+              )}
             </Stack>
           </Grid2>
         </Grid2>
       </Container>
+      <Snackbar
+        open={!!snackBarText}
+        autoHideDuration={3000}
+        onClose={() => setSnackBarText(null)}
+        message={snackBarText}
+      />
     </>
   );
 };
