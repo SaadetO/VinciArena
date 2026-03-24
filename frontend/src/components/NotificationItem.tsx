94b16a4750bc8e6de5ee307c980493ade9468fa9
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-24 14:58:19 +0100

    feat: add navigate logic to the frontend

--- frontend/src/components/NotificationItem.tsx
+++ frontend/src/components/NotificationItem.tsx
@@ -5,6 +5,7 @@ import { UserContext } from '../contexts/UserContext';
 import { MarkEmailReadOutlined } from '@mui/icons-material';
 import { formatRelativeTime } from '../utils/date';
 import { useNotifications } from '../hooks/useNotifications';
+import { NotificationContext } from '../contexts/NotificationContext';
 
 interface Props {
   notification: NotificationDto;
@@ -12,14 +13,24 @@ interface Props {
 
 export const NotificationItem = ({ notification }: Props) => {
   const { authenticatedUser } = useContext(UserContext);
+  const { handleNotificationClick } = useContext(NotificationContext);
   const { markAsRead } = useNotifications();
+  const isClickable = !!notification.idReference;
   if (!authenticatedUser) return;
   return (
     <ListItem
+      onClick={
+        isClickable ? () => handleNotificationClick(notification) : undefined
+      }
       sx={{
         backgroundColor: (theme) =>
           notification.isRead ? 'transparent' : theme.palette.background.s3,
         alignItems: 'center',
+        cursor: isClickable ? 'pointer' : 'default',
+        '&:hover': {
+          backgroundColor: (theme) =>
+            isClickable ? theme.palette.action.hover : 'transparent',
+        },
       }}
       secondaryAction={
         !notification.isRead && (
@@ -27,7 +38,11 @@ export const NotificationItem = ({ notification }: Props) => {
             <IconButton
               size="small"
               color="primary"
-              onClick={() => markAsRead(notification.idNotification)}
+              onClick={(e) => {
+                // exclude navigation when mark as read is clicked
+                e.stopPropagation();
+                markAsRead(notification.idNotification);
+              }}
             >
               <MarkEmailReadOutlined />
             </IconButton>
