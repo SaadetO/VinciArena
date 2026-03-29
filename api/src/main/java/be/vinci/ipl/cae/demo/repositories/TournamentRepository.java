21710ea484765351d556f2542192a6b3069805c0
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-03-29 17:10:58 +0200

    feat(TournamentRepo): add methods to filter tournaments by team and participating members

--- api/src/main/java/be/vinci/ipl/cae/demo/repositories/TournamentRepository.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/repositories/TournamentRepository.java
@@ -27,4 +27,21 @@ public interface TournamentRepository extends CrudRepository<Tournament, Long> {
    */
   Iterable<Tournament> findByTournamentStatusNotInOrderByStartDateDesc(
       Collection<TournamentStatus> statuses);
+
+  /**
+   * Get all tournaments where at least one of the given teams participates, from a certain date on.
+   */
+  Iterable<Tournament> findDistinctByTeams_IdTeamInOrderByStartDateDesc(Collection<Long> teamIds);
+
+  /**
+   * Get all tournaments of a certain status where at least one of the given teams participates, from a certain date on.
+   */
+  Iterable<Tournament> findDistinctByTeams_IdTeamInAndTournamentStatusOrderByStartDateDesc(
+      Collection<Long> teamIds, TournamentStatus status);
+
+  /**
+   * Get all tournaments not of a certain status where at least one of the given teams participates, from a certain date on.
+   */
+  Iterable<Tournament> findDistinctByTeams_IdTeamInAndTournamentStatusNotInOrderByStartDateDesc(
+      Collection<Long> teamIds, Collection<TournamentStatus> statuses);
 }
