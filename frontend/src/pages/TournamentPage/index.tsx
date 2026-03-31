ad832ee06fb82b0e39a15a27c74b78c09c6e7b65
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-03-31 11:54:23 +0200

    feat(Tournament): create place holderpage for tournament

---
+++ frontend/src/pages/TournamentPage/index.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useTournament } from '../../hooks/useTournament';
+import { TournamentDto } from '../../types';
+import { useParams } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { TournamentModal } from './modals/tournamentModal';
+
+export const TournamentPage = () => {
+  const { id } = useParams();
+  const [isTournamentModalOpen, setIsTournamentModalOpen] = useState(false);
+  const [tournament, setTournament] = useState<TournamentDto | null>(null);
+  const { getById } = useTournament({ setTournament });
+
+  useEffect(() => {
+    id && getById(Number(id));
+  }, [id]);
+
+  return (
+    <>
+      <p>Tournament id: {id}</p>
+      <Button onClick={() => setIsTournamentModalOpen(true)}>Modifier</Button>
+      <TournamentModal
+        open={isTournamentModalOpen}
+        onClose={() => setIsTournamentModalOpen(false)}
+        tournament={tournament ?? undefined}
+      />
+      <Button>Publier</Button>
+    </>
+  );
+};
