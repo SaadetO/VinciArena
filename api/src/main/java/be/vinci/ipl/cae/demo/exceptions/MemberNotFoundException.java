a049b09c958ebf1be19367507742d4ed23535ca9
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-11 18:50:23 +0200

    feat: add MemberNotFoundException when member is not found

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class MemberNotFoundException extends RuntimeException {
+
+  public MemberNotFoundException(String message) {
+    super(message);
+  }
+}
