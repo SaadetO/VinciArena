b16ea98dd985b1781c72bc23576be0b1b0bc30ef
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-11 18:50:54 +0200

    feat: add InvalidBanException for invalid ban operations

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/InvalidBanException.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class InvalidBanException extends RuntimeException {
+
+  public InvalidBanException(String message) {
+    super(message);
+  }
+}
