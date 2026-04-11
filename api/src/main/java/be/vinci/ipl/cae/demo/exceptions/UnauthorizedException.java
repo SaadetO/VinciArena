d0e21bf13d79d11cef771bb3df4ce2eeff0a4545
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-11 18:47:03 +0200

    feat: add UnauthorizedException for unauthenticated users

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+
+  public UnauthorizedException(String message) {
+    super(message);
+  }
+}
