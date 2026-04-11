2f1c5c59de0aed7ca05da37bccb348574bb5e94c
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-12 01:40:16 +0200

    docs: add JavaDoc to MemberNotFoundException

--- api/src/main/java/be/vinci/ipl/cae/demo/exceptions/MemberNotFoundException.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/MemberNotFoundException.java
@@ -1,7 +1,15 @@
 package be.vinci.ipl.cae.demo.exceptions;
 
+/**
+ * Exception thrown when a member is not found.
+ */
 public class MemberNotFoundException extends RuntimeException {
 
+  /**
+   * Constructor.
+   *
+   * @param message the error message
+   */
   public MemberNotFoundException(String message) {
     super(message);
   }
