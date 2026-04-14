fe08ff22b3ca805477edef8e59b1c6d09dab3747
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-14 12:44:59 +0200

    feat(match): implement confirm match result endpoint with authentication

--- api/src/main/java/be/vinci/ipl/cae/demo/controllers/MatchController.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/controllers/MatchController.java
@@ -1,10 +1,12 @@
 package be.vinci.ipl.cae.demo.controllers;
 
 import be.vinci.ipl.cae.demo.services.MatchService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,6 +28,7 @@ public class MatchController {
    * @param id the id of the match
    * @param email the authenticated user's email
    */
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   @PatchMapping("/{id}/confirm")
   public void confirmMatchResult(@PathVariable Long id,
       @AuthenticationPrincipal String email){
