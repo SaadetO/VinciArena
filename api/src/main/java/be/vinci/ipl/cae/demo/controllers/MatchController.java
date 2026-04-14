1ca9a63b31869fdd760596a08c2c8b86d98ee3b5
saadet-ozan-vinci <saadet.ozan@student.vinci.be>
2026-04-14 11:39:45 +0200

    add: create matchController file

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/controllers/MatchController.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/matchs")
+public class MatchController {}
