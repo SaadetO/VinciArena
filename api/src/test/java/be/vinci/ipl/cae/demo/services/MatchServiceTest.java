84eab81820483649a0da0867dc4dcfe598cbfcc1
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-14 12:58:00 +0200

    test: add repository mocks for MatchServiceTest

--- api/src/test/java/be/vinci/ipl/cae/demo/services/MatchServiceTest.java
+++ api/src/test/java/be/vinci/ipl/cae/demo/services/MatchServiceTest.java
@@ -1,9 +1,33 @@
 package be.vinci.ipl.cae.demo.services;
 
+import be.vinci.ipl.cae.demo.models.entities.Match;
+import be.vinci.ipl.cae.demo.models.entities.MatchResultConfirmation;
+import be.vinci.ipl.cae.demo.models.entities.Member;
+import be.vinci.ipl.cae.demo.repositories.MatchRepository;
+import be.vinci.ipl.cae.demo.repositories.MatchResultConfirmationRepository;
+import be.vinci.ipl.cae.demo.repositories.MemberRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class MatchServiceTest {
+
+  @Mock
+  private MatchRepository matchRepository;
+
+  @Mock
+  private MemberRepository memberRepository;
+
+  @Mock
+  private MatchResultConfirmationRepository confirmationRepository;
+
+  @InjectMocks
+  private MatchService matchService;
+
+  private Match match;
+  private Member member;
+  private MatchResultConfirmation confirmation;
 
 }
