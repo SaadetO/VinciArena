f54c238aef651db5834550eb37b4285d74b59e3a
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-14 12:37:25 +0200

    feat(match): inject MatchService into MatchController

--- api/src/main/java/be/vinci/ipl/cae/demo/controllers/MatchController.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/controllers/MatchController.java
@@ -1,5 +1,6 @@
 package be.vinci.ipl.cae.demo.controllers;
 
+import be.vinci.ipl.cae.demo.services.MatchService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/matches")
-public class MatchController {}
+public class MatchController {
+
+  private final MatchService matchService;
+
+  public MatchController(MatchService matchService){
+    this.matchService = matchService;
+  }
+
+
+
+}
