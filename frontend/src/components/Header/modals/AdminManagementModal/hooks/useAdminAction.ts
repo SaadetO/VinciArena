23e314f14ca18028ffc72515362b7b4dd20abe03
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-14 02:12:41 +0200

    feat(ban): fetch isLastMember in useAdminAction before opening modal

--- frontend/src/components/Header/modals/AdminManagementModal/hooks/useAdminAction.ts
+++ frontend/src/components/Header/modals/AdminManagementModal/hooks/useAdminAction.ts
@@ -15,10 +15,11 @@ export const useAdminAction = () => {
 
   const [users, setUsers] = useState<Member[]>([]);
   const [pendingIds, setPendingIds] = useState<number[]>([]);
-  const { getAll, toggleAdmin, isGettingUsers, banMember } = useMembers({
-    setUsers,
-    setPendingIds,
-  });
+  const { getAll, toggleAdmin, isGettingUsers, banMember, checkIsLastMember } =
+    useMembers({
+      setUsers,
+      setPendingIds,
+    });
 
   const handleToggleAdmin = async (user: Member) => {
     if (user.id === authenticatedUser?.id) {
@@ -34,7 +35,10 @@ export const useAdminAction = () => {
     toggleAdmin(user.id, user.admin);
   };
 
-  const handleBan = (id: number, tag: string) => {
+  const handleBan = async (id: number, tag: string) => {
+    const isLast = await checkIsLastMember(id);
+
+    console.log('isLastMember =', isLast);
     openModal(
       banModal({
         tag,
