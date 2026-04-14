790cb1e250f1041beee1baaed86ed858f7e68d6c
alexane-dubois-vinci <alexane.dubois@student.vinci.be>
2026-04-14 20:17:55 +0200

    feat(exceptions): add InvalidCredentialsException

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+  public InvalidCredentialsException(String message) {
+    super(message);
+  }
+}
