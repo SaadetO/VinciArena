5420c642fcaa3396f4bdfdd18df20a7a39b90cee
vinci-megane-walschot <megane.walschot@student.vinci.be>
2026-04-14 11:28:03 +0200

    feat(api): add match service

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/services/MatchService.java
@@ -0,0 +1,34 @@
+package be.vinci.ipl.cae.demo.services;
+
+import be.vinci.ipl.cae.demo.repositories.MatchRepository;
+import be.vinci.ipl.cae.demo.repositories.MemberRepository;
+import be.vinci.ipl.cae.demo.repositories.TeamRepository;
+import be.vinci.ipl.cae.demo.repositories.TournamentRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * Match service.
+ */
+@Service
+public class MatchService {
+  private  final MatchRepository matchRepository;
+  private  final TeamRepository teamRepository;
+  private final MemberRepository memberRepository;
+  private final TournamentRepository tournamentRepository;
+
+  /**
+   * Constructor.
+   *
+   * @param matchRepository the match repository
+   * @param teamRepository the team repository
+   * @param memberRepository the member repository
+   * @param tournamentRepository the tournament repository
+   */
+  public MatchService(MatchRepository matchRepository, TeamRepository teamRepository,
+      MemberRepository memberRepository, TournamentRepository tournamentRepository) {
+    this.matchRepository = matchRepository;
+    this.teamRepository = teamRepository;
+    this.memberRepository = memberRepository;
+    this.tournamentRepository = tournamentRepository;
+  }
+}
