f69cacad8dc29d21bf2a82c2d1e1527411c81b48
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-15 14:11:25 +0200

    style: add javadoc for custom match exceptions

--- api/src/main/java/be/vinci/ipl/cae/demo/exceptions/UserNotInMatchException.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/UserNotInMatchException.java
@@ -1,7 +1,15 @@
 package be.vinci.ipl.cae.demo.exceptions;
 
+/**
+ * Exception thrown when a user is not part of a match.
+ */
 public class UserNotInMatchException extends RuntimeException {
 
+  /**
+   * Constructor.
+   *
+   * @param message the error message
+   */
   public UserNotInMatchException(String message) {
     super(message);
   }
