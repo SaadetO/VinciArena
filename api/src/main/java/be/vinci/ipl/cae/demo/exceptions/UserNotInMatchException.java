a5d0de05286ea2cd9d510a440d40da84cdb6e119
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-15 14:01:55 +0200

    feat: add UserNotInMatchException for users not part of a match

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/UserNotInMatchException.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class UserNotInMatchException extends RuntimeException {
+
+  public UserNotInMatchException(String message) {
+    super(message);
+  }
+}
