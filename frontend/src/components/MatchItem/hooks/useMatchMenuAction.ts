c2e9ab391efb031800283b8b4827b79046adf7ef
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-15 20:39:33 +0200

    feat(match): add api and context imports to match menu hook

--- frontend/src/components/MatchItem/hooks/useMatchMenuAction.ts
+++ frontend/src/components/MatchItem/hooks/useMatchMenuAction.ts
@@ -1,15 +1,17 @@
+import { useApi } from '../../../hooks/useApi';
+import { useSnackbar } from '../../../hooks/useSnackbar';
+import { useContext } from 'react';
+import { UserContext } from '../../../contexts/UserContext';
+import { ApiError } from '../../../types';
+
 export const useMatchMenuAction = () => {
   const handleForfeit = () => {};
 
   const handleEditComposition = () => {};
 
-  const handleContestScore = (matchId: number) => {
+  const handleContestScore = (matchId: number) => {};
 
-  };
-
-  const handleConfirmScore = (matchId: number) => {
-    
-  };
+  const handleConfirmScore = (matchId: number) => {};
 
   const handleEncodeScore = () => {};
 
