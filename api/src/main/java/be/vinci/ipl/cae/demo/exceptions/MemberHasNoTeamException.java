0efd801638c1aef50b3a1b7c4031f82f8e7d0fde
asma-mellah-vinci <asma.mellah@student.vinci.be>
2026-04-15 14:00:59 +0200

    feat: add MemberHasNoTeamException for members without team

---
+++ api/src/main/java/be/vinci/ipl/cae/demo/exceptions/MemberHasNoTeamException.java
@@ -0,0 +1,8 @@
+package be.vinci.ipl.cae.demo.exceptions;
+
+public class MemberHasNoTeamException extends RuntimeException {
+
+  public MemberHasNoTeamException(String message) {
+    super(message);
+  }
+}
