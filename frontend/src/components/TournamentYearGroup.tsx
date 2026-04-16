6c9143fd30dd0738bc97bc90b687530417228efc
NiteChill <puissantachille@gmail.com>
2026-04-16 22:55:07 +0200

    refactor: shrinks down year header for tournament and matches section

--- frontend/src/components/TournamentYearGroup.tsx
+++ frontend/src/components/TournamentYearGroup.tsx
@@ -17,13 +17,16 @@ export const TournamentYearGroup = ({
         position="sticky"
         top="8rem"
         zIndex={1}
-        bgcolor="background.s0"
         width="fit-content"
         borderRadius="0.75rem"
         padding=" 0.5rem 1rem"
         ml="0.75rem !important"
+        sx={{
+          background: (theme) => theme.palette.background.s0,
+          boxShadow: '0 8px 32px rgba(0, 0, 0, 0.4)',
+        }}
       >
-        <Typography variant="h2">{year}</Typography>
+        <Typography variant="h3">{year}</Typography>
       </Stack>
       {monthsData.map((monthData) => (
         <TournamentMonthGroup
