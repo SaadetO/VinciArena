8b994c4ca8bae9900948a03fc5c31ac187c28618
NiteChill <puissantachille@gmail.com>
2026-04-24 02:05:52 +0200

    feat: send specialty and avatar to the backend for team display and player display

--- api/src/main/java/be/vinci/ipl/cae/demo/services/MemberService.java
+++ api/src/main/java/be/vinci/ipl/cae/demo/services/MemberService.java
@@ -63,11 +63,11 @@ public class MemberService {
   /**
    * Constructor.
    *
-   * @param passwordEncoder          the password encoder
-   * @param memberRepository         the member repository
+   * @param passwordEncoder the password encoder
+   * @param memberRepository the member repository
    * @param unavailabilityRepository the unavailability repository
-   * @param specialtyRepository      the specialty repository
-   * @param profileImageRepository   the profile image repository
+   * @param specialtyRepository the specialty repository
+   * @param profileImageRepository the profile image repository
    */
   public MemberService(
       BCryptPasswordEncoder passwordEncoder,
@@ -90,7 +90,7 @@ public class MemberService {
    * Create an AuthenticatedUser based on a member and a token.
    *
    * @param member the member
-   * @param token  the token
+   * @param token the token
    * @return the authenticated user
    */
   public AuthenticatedUser toAuthenticatedUser(Member member, String token) {
@@ -149,7 +149,7 @@ public class MemberService {
   /**
    * Login a member.
    *
-   * @param email    the member email
+   * @param email the member email
    * @param password the member password
    * @return the authenticated user if login succeeds
    */
@@ -246,7 +246,7 @@ public class MemberService {
   /**
    * Update a member's profile image.
    *
-   * @param member       the member
+   * @param member the member
    * @param profileImage the new profile image
    * @return true if updated, false if the image is invalid
    */
@@ -267,7 +267,7 @@ public class MemberService {
   /**
    * Update a member's profile image.
    *
-   * @param member      the member
+   * @param member the member
    * @param specialtyId the new profile image
    * @return true if updated, false if the image is invalid
    */
@@ -287,7 +287,7 @@ public class MemberService {
   /**
    * Get member profile DTO with privacy rules.
    *
-   * @param requestedId        the requested member ID
+   * @param requestedId the requested member ID
    * @param authenticatedEmail the authenticated user email
    * @return the profile DTO or null if not found
    */
@@ -482,11 +482,11 @@ public class MemberService {
   /**
    * Validate business rules before banning a member.
    *
-   * @param member    the member to ban
+   * @param member the member to ban
    * @param requester the member performing the action
-   * @throws CannotBanAdminException      if trying to ban an admin
+   * @throws CannotBanAdminException if trying to ban an admin
    * @throws MemberAlreadyBannedException if the member is already banned
-   * @throws CannotBanSelfException       if the user tries to ban themselves
+   * @throws CannotBanSelfException if the user tries to ban themselves
    */
   private void checkBanValidity(Member member, Member requester) {
     if (member.getIdMember().equals(requester.getIdMember())) {
@@ -560,14 +560,14 @@ public class MemberService {
   /**
    * Ban a member from the platform (soft delete).
    *
-   * @param id             the ID of the member to ban
+   * @param id the ID of the member to ban
    * @param requesterEmail the email of the authenticated user
-   * @throws NotAuthenticatedException    if the user is not authenticated
-   * @throws NotAdminException            if the requester is not an admin
-   * @throws MemberNotFoundException      if the member does not exist
-   * @throws CannotBanAdminException      if trying to ban an admin
+   * @throws NotAuthenticatedException if the user is not authenticated
+   * @throws NotAdminException if the requester is not an admin
+   * @throws MemberNotFoundException if the member does not exist
+   * @throws CannotBanAdminException if trying to ban an admin
    * @throws MemberAlreadyBannedException if the member is already banned
-   * @throws CannotBanSelfException       if the user tries to ban themselves
+   * @throws CannotBanSelfException if the user tries to ban themselves
    */
   @Transactional
   public void banMember(Long id, String requesterEmail) {
