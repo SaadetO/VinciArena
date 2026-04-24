8b192f5b2d87bbb13efd1dc65ce8e2ea6028c583
NiteChill <puissantachille@gmail.com>
2026-04-24 18:31:15 +0200

    feat: implement tournamentSection component inside homepage

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,9 +1,51 @@
 import { Container, Stack } from '@mui/material';
+import { TournamentSection } from '../../components/TournamentSection';
+import { useHomePage } from './hooks/useHomePage';
+import { useEffect } from 'react';
 
 export const HomePage = () => {
+  const {
+    futureTournaments,
+    currentTournaments,
+    pastTournaments,
+    isGettingFutureTournaments,
+    isGettingCurrentTournaments,
+    isGettingPastTournaments,
+    fetchTournaments,
+  } = useHomePage();
+
+  useEffect(() => {
+    fetchTournaments();
+  }, [fetchTournaments]);
+
   return (
-    <Container component={Stack} spacing="2rem" maxWidth="md">
-      hello
+    <Container
+      component={Stack}
+      spacing="2rem"
+      padding="1.5rem 0 4rem 0"
+      maxWidth="md"
+    >
+      <TournamentSection
+        title="Tournois à venir"
+        isLoading={isGettingFutureTournaments}
+        tournaments={futureTournaments}
+        emptyMessage="Aucun tournoi à venir."
+        timeFrame="future"
+      />
+      <TournamentSection
+        title="Tournois en cours"
+        isLoading={isGettingCurrentTournaments}
+        tournaments={currentTournaments}
+        emptyMessage="Aucun tournoi en cours."
+        timeFrame="current"
+      />
+      <TournamentSection
+        title="Tournois passés"
+        isLoading={isGettingPastTournaments}
+        tournaments={pastTournaments}
+        emptyMessage="Aucun tournoi passé."
+        timeFrame="past"
+      />
     </Container>
   );
 };
